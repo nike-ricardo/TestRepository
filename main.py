4b871bf161920b0cc534bb7d92cce1d04ad9f4c8
nike-ricardo <151964152+nike-ricardo@users.noreply.github.com>
2023-11-27 15:56:40 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 def main:
   print("Hello World")
-  print("I will test my Slack messages. Let me test, please. I have tried a lot.")
+  print("Well, at least I finally am able to receive the message")
